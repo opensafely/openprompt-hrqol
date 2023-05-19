dce32bfa3dacc94afff7e708b4d44b714b9d7904
Ali Henderson <alasdair.henderson1@lshtm.ac.uk>
2023-05-19 18:23:11 +0100

    Change databuilder to run OpenPROMPT and TPP extracts
    created a separate databuilder code that should read in OpenPROMPT data, then extract the TPP variables if they are in a TPP practice.
    
    Cannot get it to work with the @table_from_file code for now, will revisit

--- analysis/dataset_definition_lc.py
+++ analysis/dataset_definition_lc.py
@@ -18,87 +18,91 @@ from variable_lib import (
 import datetime
 import codelists
 
-dataset = Dataset()
-
 # Registered at a GP 3 months prior to start date
 study_start_date = datetime.date(2020, 3, 1)
 study_end_date = datetime.date(2023, 5, 1)
 minimum_registration = 90
 
-# Restrict to people with one practice & for 3month before start date
-registrations = practice_registrations \
-    .where(practice_registrations.start_date <= study_start_date - days(minimum_registration)) \
-    .except_where(practice_registrations.end_date <= study_start_date)
 
-# Get the number of registrations in this period to exclude anyone with >1 in 'set-population later'
-registrations_number = registrations.count_for_patient()
+def add_tpp_data(dataset, population):
+    # Restrict to people with one practice & for 3month before start date
+    registrations = practice_registrations \
+        .except_where(practice_registrations.end_date <= study_start_date)
 
-# Need to get the start and end date of last registration only
-registration = registrations \
-    .sort_by(practice_registrations.start_date).last_for_patient()
+    # Get the number of registrations in this period to exclude anyone with >1 in 'set-population later'
+    registrations_number = registrations.count_for_patient()
 
-dataset.pt_start_date = case(
-    when(registration.start_date + days(minimum_registration) > study_start_date).then(registration.start_date + days(minimum_registration)),
-    default=study_start_date,
-)
+    # Need to get the start and end date of last registration only
+    registration = registrations \
+        .sort_by(practice_registrations.start_date).last_for_patient()
 
-dataset.pt_end_date = case(
-    when(registration.end_date.is_null()).then(study_end_date),
-    when(registration.end_date > study_end_date).then(study_end_date),
-    default=registration.end_date,
-)
+    dataset.pt_start_date = case(
+        when(registration.start_date + days(minimum_registration) > study_start_date).then(registration.start_date + days(minimum_registration)),
+        default=study_start_date,
+    )
 
-dataset.start_date = registration.start_date
-dataset.end_date = registration.end_date
+    dataset.pt_end_date = case(
+        when(registration.end_date.is_null()).then(study_end_date),
+        when(registration.end_date > study_end_date).then(study_end_date),
+        default=registration.end_date,
+    )
 
-# get NHS region one by one
-dataset.practice_nuts = registration.practice_nuts1_region_name
+    dataset.start_date = registration.start_date
+    dataset.end_date = registration.end_date
 
-# Common codes
-# Demographic variables
-dataset.sex = patients.sex
-dataset.age = age_as_of(study_start_date)
-dataset.has_died = has_died(study_start_date)
-dataset.msoa = address_as_of(study_start_date).msoa_code
-dataset.imd = address_as_of(study_start_date).imd_rounded
-dataset.death_date = patients.date_of_death
+    # get NHS region one by one
+    dataset.practice_nuts = registration.practice_nuts1_region_name
 
-# Ethnicity in 6 categories
-dataset.ethnicity = clinical_events.where(clinical_events.ctv3_code.is_in(codelists.ethnicity)) \
-    .sort_by(clinical_events.date) \
-    .last_for_patient() \
-    .ctv3_code.to_category(codelists.ethnicity)
+    # Common codes
+    # Demographic variables
+    dataset.sex = patients.sex
+    dataset.age = age_as_of(study_start_date)
+    dataset.has_died = has_died(study_start_date)
+    dataset.msoa = address_as_of(study_start_date).msoa_code
+    dataset.imd = address_as_of(study_start_date).imd_rounded
+    dataset.death_date = patients.date_of_death
 
-# Covid tests
-all_test_positive = sgss_covid_all_tests \
-    .where(sgss_covid_all_tests.is_positive) \
-    .except_where(sgss_covid_all_tests.specimen_taken_date <= dataset.pt_start_date) \
-    .except_where(sgss_covid_all_tests.specimen_taken_date >= dataset.pt_end_date)
+    # Ethnicity in 6 categories
+    dataset.ethnicity = clinical_events.where(clinical_events.ctv3_code.is_in(codelists.ethnicity)) \
+        .sort_by(clinical_events.date) \
+        .last_for_patient() \
+        .ctv3_code.to_category(codelists.ethnicity)
 
-dataset.all_test_positive = all_test_positive.count_for_patient()
+    # Covid tests
+    all_test_positive = sgss_covid_all_tests \
+        .where(sgss_covid_all_tests.is_positive) \
+        .except_where(sgss_covid_all_tests.specimen_taken_date <= dataset.pt_start_date) \
+        .except_where(sgss_covid_all_tests.specimen_taken_date >= dataset.pt_end_date)
 
-dataset.all_tests = sgss_covid_all_tests \
-    .except_where(sgss_covid_all_tests.specimen_taken_date <= dataset.pt_start_date) \
-    .except_where(sgss_covid_all_tests.specimen_taken_date >= dataset.pt_end_date) \
-    .count_for_patient()
+    dataset.all_test_positive = all_test_positive.count_for_patient()
 
-# Find any records of long COVID
-long_covid = clinical_events \
-    .where(clinical_events.snomedct_code.is_in(codelists.long_covid_combined))
+    dataset.all_tests = sgss_covid_all_tests \
+        .except_where(sgss_covid_all_tests.specimen_taken_date <= dataset.pt_start_date) \
+        .except_where(sgss_covid_all_tests.specimen_taken_date >= dataset.pt_end_date) \
+        .count_for_patient()
 
-# Split them by Dx and Rx
-dataset.long_covid_first_dx = long_covid \
-    .where(long_covid.snomedct_code.is_in(codelists.long_covid_dx_codes)) \
-    .sort_by(long_covid.date) \
-    .first_for_patient().date
+    # Find any records of long COVID
+    long_covid = clinical_events \
+        .where(clinical_events.snomedct_code.is_in(codelists.long_covid_combined))
 
-dataset.long_covid_n_rx = long_covid \
-    .where(long_covid.snomedct_code.is_in(codelists.long_covid_referral_codes)) \
-    .where(long_covid.date > datetime.date(2020, 12, 31)) \
-    .where(long_covid.date >= dataset.long_covid_first_dx) \
-    .count_for_patient()
+    # Split them by Dx and Rx
+    dataset.long_covid_first_dx = long_covid \
+        .where(long_covid.snomedct_code.is_in(codelists.long_covid_dx_codes)) \
+        .sort_by(long_covid.date) \
+        .first_for_patient().date
 
-# Apply some restrictions
-# and output a dataset
-population_restriction = (dataset.age > 0) & (dataset.age < 110) & (registrations_number == 1)
-dataset.define_population(population_restriction)
+    dataset.long_covid_first_rx = long_covid \
+        .where(long_covid.snomedct_code.is_in(codelists.long_covid_referral_codes)) \
+        .sort_by(long_covid.date) \
+        .first_for_patient().date
+
+    dataset.long_covid_n_rx = long_covid \
+        .where(long_covid.snomedct_code.is_in(codelists.long_covid_referral_codes)) \
+        .where(long_covid.date > datetime.date(2020, 12, 31)) \
+        .where(long_covid.date >= dataset.long_covid_first_dx) \
+        .count_for_patient()
+
+    # Apply some restrictions
+    # and output a dataset
+    population_restriction = (dataset.age > 0) & (dataset.age < 110) & (registrations_number == 1)
+    dataset.define_population(population_restriction)
---
+++ analysis/dataset_definition_openprompt.py
@@ -0,0 +1,41 @@
+from databuilder.ehrql import Dataset, days, case, when
+from databuilder.query_language import table_from_file, PatientFrame, Series 
+from databuilder.tables.beta.tpp import (
+#  OpenPROMPT,
+  patients,
+  practice_registrations
+)
+from dataset_definition_lc import add_tpp_data
+from datetime import date
+
+
+@table_from_file("output/dummy_openprompt.csv")
+class OpenPROMPT(PatientFrame):
+    sex = Series(str)
+    age = Series(int)
+    number_surveys = Series(int)
+    long_covid_date = Series(str)
+    breathlessness = Series(int)
+    fatigue = Series(int)
+    productivity = Series(int)
+
+
+dataset = Dataset()
+registration = practice_registrations
+# this currently only allows for an & not | in the last row
+# so we exclude anyone without TPP in OpenPROMPT as it stands
+dataset.define_population(
+    (patients.age_on(date.today()) >= 18)
+    & registration.exists_for_patient()
+    & OpenPROMPT.exists_for_patient()
+)
+
+add_tpp_data(dataset, population=patients.exists_for_patient())
+
+dataset.op_sex = OpenPROMPT.sex
+dataset.op_age = OpenPROMPT.age
+dataset.op_number_surveys = OpenPROMPT.number_surveys
+dataset.op_long_covid_date = OpenPROMPT.long_covid_date
+dataset.op_breathlessness = OpenPROMPT.breathlessness
+dataset.op_fatigue = OpenPROMPT.fatigue
+dataset.op_productivity = OpenPROMPT.productivity
