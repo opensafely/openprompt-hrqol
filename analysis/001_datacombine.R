b70a0287d28435f57e7fc1fa6abcedc7cad371e0
Ali Henderson <alasdair.henderson1@lshtm.ac.uk>
2023-06-14 17:01:47 +0100

    remove filtering step and summarise raw data

--- analysis/001_datacombine.R
+++ analysis/001_datacombine.R
@@ -71,6 +71,15 @@ op_survey3 <- read_csv(here("output/openprompt_survey3.csv"), col_types = resear
 op_survey4 <- read_csv(here("output/openprompt_survey4.csv"), col_types = research_col_spec) %>% 
   mutate(survey_response = 4)
 
+
+# output raw data summaries -----------------------------------------------
+summarise_data(data_in = op_baseline, filename = "op_baseline")
+summarise_data(data_in = op_survey1, filename = "op_survey1")
+summarise_data(data_in = op_survey2, filename = "op_survey2")
+summarise_data(data_in = op_survey3, filename = "op_survey3")
+summarise_data(data_in = op_survey4, filename = "op_survey4")
+
+
 # stack research questionnaire responses ----------------------------------
 op_surveys <- bind_rows(
   op_survey1, 
@@ -108,13 +117,7 @@ dev.off()
 write.csv(op_raw, here::here("output/openprompt_raw.csv.gz"))
 
 # Filter out missing data:  -----------------------------------------------
-op_filtered <- op_raw %>% 
-  # Filter out if survey_date does not exist
-  filter(!is.na(survey_date)) %>% 
-  # Filter if NA response to disability question (compulsory Q on the baseline Q'airre so an indication that they did not complete any survey) %>% 
-  filter(!is.na(disability)) %>% 
-  # Filter if NA response to EQ-5D question: compulsory on the Research questionnaire 
-  filter(!is.na(eq5d_usualactivities))
+op_filtered <- op_raw
 
 # map ctv3codes to the description ----------------------------------------
 op_mapped <- op_filtered %>% 
