446324502856ff54c0fd501382adc97ff85c981d
Ali Henderson <alasdair.henderson1@lshtm.ac.uk>
2023-06-14 16:15:03 +0100

    create dummy open_prompt table

---
+++ analysis/dummy_data_editing/edit_automatic_dummy_data.R
@@ -0,0 +1,132 @@
+#' Edit the dummy data file created by ehrQL
+#' 
+#' Run the following in command line before executing this code
+#' opensafely exec ehrql:v0 create-dummy-tables analysis/model_questions/process_baseline.py output/dummydata -- --day=0
+
+library(tidyverse)
+library(here)
+
+source(here::here("analysis/dummy_data_editing/baseline_questions.R"))
+source(here::here("analysis/dummy_data_editing/research_questions.R"))
+
+op_baseline <- read_csv(here::here("output/dummydata/open_prompt.csv"))
+
+questions <- bind_rows(questions_baseline, questions_research)
+
+get_ctv3_or_numeric <- function(qq, return_value = "ctv3"){
+  subset_qs <- questions[questions$id == qq,]
+  if(subset_qs[1, "value_property"] == "ctv3_code"){
+    num_val <- 0
+    ctv3 <- sample(subset_qs$ctv3_codes, 1)
+  }else{
+    num_val <- sample(0:10, size = 1)
+    ctv3 <- subset_qs[1, "ctv3_codes"]
+  }
+  return(get(return_value))
+}
+
+unique_ids <- unique(op_baseline$patient_id)
+# number of patients in the dummy data
+n_patid <- length(unique_ids)
+set.seed(1320523756)
+
+# generate a random response to every questions for day 0 
+dummy_data <- NULL
+for(patid in unique_ids){
+  dummy_data <- bind_rows(
+    dummy_data, 
+    bind_cols(
+      patient_id = patid,
+      consultation_date = as.Date("2022-11-01") + sample(1:180, 1),
+      consultation_id = round(runif(1, 531513,5314141)),
+      ctv3_code = sapply(unique(questions$id), function(xx){get_ctv3_or_numeric(xx, return_value = "ctv3")}),
+      numeric_value = sapply(unique(questions$id), function(xx){get_ctv3_or_numeric(xx, return_value = "num_val")})
+      )
+  )
+}
+
+# generate research survey responses 25-35 days after day 0 
+dummy_data_survey2 <- NULL 
+previous_ids <- unique(dummy_data$patient_id)
+sample_ids <- sample(previous_ids, size = length(previous_ids) * 0.4)
+for(patid in sample_ids){
+  dummy_data_survey2 <- bind_rows(
+    dummy_data_survey2,
+    bind_cols(
+      patient_id = patid, 
+      consultation_date = pull(dummy_data[dummy_data$patient_id==patid, "consultation_date"])[1] + runif(1, 25, 35),
+      consultation_id = round(runif(1, 531513,5314141)),
+      ctv3_code = sapply(unique(questions_research$id), function(xx){get_ctv3_or_numeric(xx, return_value = "ctv3")}),
+      numeric_value = sapply(unique(questions_research$id), function(xx){get_ctv3_or_numeric(xx, return_value = "num_val")})
+      )
+  )
+}
+
+# generate research survey responses 25-35 days after day 0 
+dummy_data_survey3 <- NULL 
+previous_ids <- unique(dummy_data_survey2$patient_id)
+sample_ids <- sample(previous_ids, size = length(previous_ids) * 0.5)
+for(patid in sample_ids){
+  dummy_data_survey3 <- bind_rows(
+    dummy_data_survey3,
+    bind_cols(
+      patient_id = patid, 
+      consultation_date = pull(dummy_data[dummy_data$patient_id==patid, "consultation_date"])[1] + runif(1, 55, 65),
+      consultation_id = round(runif(1, 531513,5314141)),
+      ctv3_code = sapply(unique(questions_research$id), function(xx){get_ctv3_or_numeric(xx, return_value = "ctv3")}),
+      numeric_value = sapply(unique(questions_research$id), function(xx){get_ctv3_or_numeric(xx, return_value = "num_val")})
+    )
+  )
+}
+
+# generate research survey responses 25-35 days after day 0 
+dummy_data_survey4 <- NULL 
+previous_ids <- unique(dummy_data_survey3$patient_id)
+sample_ids <- sample(previous_ids, size = length(previous_ids) * 0.5)
+for(patid in sample_ids){
+  dummy_data_survey4 <- bind_rows(
+    dummy_data_survey4,
+    bind_cols(
+      patient_id = patid, 
+      consultation_date = pull(dummy_data[dummy_data$patient_id==patid, "consultation_date"])[1] + runif(1, 85, 95),
+      consultation_id = round(runif(1, 531513,5314141)),
+      ctv3_code = sapply(unique(questions_research$id), function(xx){get_ctv3_or_numeric(xx, return_value = "ctv3")}),
+      numeric_value = sapply(unique(questions_research$id), function(xx){get_ctv3_or_numeric(xx, return_value = "num_val")})
+    )
+  )
+}
+
+
+dummy_data_combined <- bind_rows(
+  dummy_data, 
+  dummy_data_survey2,
+  dummy_data_survey3,
+  dummy_data_survey4
+)
+
+
+# some final manual bodge edits on the numeric_value
+new_euroqol <- dummy_data_combined[dummy_data_combined$ctv3_code == "XaZ2m", "numeric_value"] * 9 + round(runif(1, 0, 10))
+dummy_data_combined[dummy_data_combined$ctv3_code == "XaZ2m", "numeric_value"] <- new_euroqol
+
+# Eq5d should be between 1-5
+eq5d_row_ids <- dummy_data_combined$ctv3_code %in% c("XaYwl", "XaYwm", "XaYwn", "XaYwo", "XaYwp") 
+new_eq5d <- sample(x = 1:5, sum(eq5d_row_ids), replace = TRUE)
+dummy_data_combined[eq5d_row_ids, "numeric_value"] <- new_eq5d
+
+# create some missingness 
+optional_questions <- c("Y3a80",
+                        "Y3a97",
+                        "Y3a98",
+                        "Y3a99",
+                        "Y3a9a",
+                        "Y3a7f",
+                        "Y3a9e",
+                        "Y3a9f",
+                        "Y3aa0")
+optional_response_rows <- which(dummy_data_combined$ctv3_code %in% optional_questions)
+# delete some of the optional responses:
+dummy_data_combined <- dummy_data_combined[-sample(optional_response_rows, size = length(optional_response_rows)*0.4), ]
+
+# export this mess --------------------------------------------------------
+write_csv(dummy_data_combined, here::here("output/dummydata/dummy_edited/open_prompt.csv"))
---
+++ analysis/dummy_data_editing/research_questions.R
@@ -0,0 +1,254 @@
+Question <- function(id, ctv3_codes, value_property){
+  data.frame(
+    id = rep(id, length(ctv3_codes)),
+    ctv3_codes, 
+    value_property = rep(value_property, length(ctv3_codes))
+  )
+}
+
+questions_research <- bind_rows(
+    Question(
+    "long_covid", # "Do you think that you currently have or have ever had COVID-19?
+    c(
+      "Y26b2",
+      "Y31ce",
+      "Y25a5",
+      "Y3a94",
+      "Y3a95",
+      "Y3a96"
+    ),
+    "ctv3_code"
+  ),
+  Question(
+    "first_covid", # "When do you think you first got (or might have got) COVID-19? If you do not remember exactly, please put your best estimate.
+    c(
+      "Y3a97"
+    ),
+    "numeric_value"
+  ),
+  Question( 
+    "n_covids", # "How many times do you think you have had an episode of COVID-19?
+    c(
+      "Y3a98"
+    ),
+    "numeric_value" 
+  ),
+  Question(
+    "recovered_from_covid",
+    c(
+      "Y3a99",
+      "Y3a9a"
+    ),
+    "ctv3_code"
+  ),
+  Question(
+    "covid_duration",
+    c(
+      "Y3a7f"
+    ),
+    "numeric_value"
+  ),
+  Question(
+    "vaccinated",
+    c(
+      "Y3a9b",
+      "Y3a9c",
+      "Y3a9d"
+    ),
+    "ctv3_code"
+  ),
+  Question(
+    "n_vaccines",
+    c(
+      "Y3a9e"
+    ),
+    "numeric_value"
+  ),
+  Question(
+    "first_vaccine_date",
+    c(
+      "Y3a9f"
+    ),
+    "numeric_value" # FIXME: should be date
+  ),
+  Question(
+    "most_recent_vaccine_date",
+    c(
+      "Y3aa0"
+    ),
+    "numeric_value" # FIXME: should be date
+  ),
+  Question( 
+    "eq5d_mobility", # "How many times do you think you have had an episode of COVID-19?
+    c(
+      "XaYwl"
+    ),
+    "numeric_value" # should be 0-5
+  ),
+  Question( 
+    "eq5d_selfcare", # "How many times do you think you have had an episode of COVID-19?
+    c(
+      "XaYwm"
+    ),
+    "numeric_value" # should be 0-5
+  ),
+  Question( 
+    "eq5d_usualactivities", # "How many times do you think you have had an episode of COVID-19?
+    c(
+      "XaYwo"
+    ),
+    "numeric_value" # should be 0-5
+  ),
+  Question( 
+    "eq5d_pain_discomfort", # "How many times do you think you have had an episode of COVID-19?
+    c(
+      "XaYwp"
+    ),
+    "numeric_value" # should be 0-5
+  ),
+  Question( 
+    "eq5d_anxiety_depression", # "How many times do you think you have had an episode of COVID-19?
+    c(
+      "XaYwr"
+    ),
+    "numeric_value" # should be 0-5
+  ),
+  Question(
+    "EuroQol_score",
+    c(
+      "XaZ2m"
+    ),
+    "numeric_value"
+  ),
+  Question(
+    "employment_status",
+    c(
+      "Ua0TB",
+      "13J7."
+    ),
+    "ctv3_code"
+  ),
+  # Question( FIXME: Ua0pu used for three questions, can't disambiguate 
+  #     "hours_worked",
+  #     c(
+  #         "Ua0pu",
+  #     ),
+  #     "numeric_value"
+  # ),
+  Question(
+    "work_affected",
+    c(
+      "Y3a80"
+    ),
+    "numeric_value"
+  ),
+  Question(
+    "life_affected",
+    c(
+      "Y3a81"
+    ),
+    "numeric_value"
+  ),
+  Question(
+    "facit_fatigue",
+    c(
+      "Y3a82"
+    ),
+    "numeric_value"
+  ), 
+  Question(
+    "facit_weak",
+    c(
+      "Y3a83" 
+    ),
+    "numeric_value"
+  ), 
+  Question(
+    "facit_listless",
+    c(
+      "Y3a84" 
+    ),
+    "numeric_value"
+  ), 
+  Question(
+    "facit_tired",
+    c(
+      "Y3a85" 
+    ),
+    "numeric_value"
+  ), 
+  Question(
+    "facit_trouble_starting",
+    c(
+      "Y3a86"
+    ),
+    "numeric_value"
+  ),
+  Question(
+    "facit_trouble_finishing",
+    c(
+      "Y3a87"
+    ),
+    "numeric_value"
+  ), 
+  Question(
+    "facit_energy",
+    c(
+      "Y3a88"
+    ),
+    "numeric_value"
+  ), 
+  Question(
+    "facit_usual_activities",
+    c(
+      "Y3a89"
+    ),
+    "numeric_value"
+  ), 
+  Question(
+    "facit_sleep_during_day",
+    c(
+      "Y3a8a"
+    ),
+    "numeric_value"
+  ),
+  Question(
+    "facit_eat",
+    c(
+      "Y3a8b"
+    ),
+    "numeric_value"
+  ),
+  Question(
+    "facit_need_help",
+    c(
+      "Y3a8c"
+    ),
+    "numeric_value"
+  ),
+  Question(
+    "facit_frustrated",
+    c(
+      "Y3a8d"
+    ),
+    "numeric_value"
+  ),
+  Question(
+    "facit_limit_social_activity",
+    c(
+      "Y3a8e"
+    ),
+    "numeric_value"
+  ),
+  Question(
+    "mrc_breathlessness",
+    c(
+      "XaIUi",
+      "XaIUl",
+      "XaIUm",
+      "XaIUn",
+      "XaIUo"    
+    ),
+    "ctv3_code"
+  ),
+)
